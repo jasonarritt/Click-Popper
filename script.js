81e6ba5fad15462fde0b3268e4dea1dfbae19ff5
Jason Arritt <jsn.arritt@gmail.com>
2023-11-08 15:08:12 -0500

    Base selecrtors added

---
+++ script.js
@@ -0,0 +1,10 @@
+const playArea = {};
+const player = {};
+
+playArea.stats = document.querySelector('.stats');
+playArea.main = document.querySelector('.main');
+playArea.game = document.querySelector('.main');
+playArea.btns = document.querySelectorAll('.btn');
+Array.from(document.querySelectorAll('.btn'));
+Array.from(document.querySelectorAll('.page'));
+console.log(playArea);
