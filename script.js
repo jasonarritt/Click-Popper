00445841b32ffd3536a70ee780074aafbefb528c
Jason Arritt <jsn.arritt@gmail.com>
2023-11-09 18:17:31 -0500

    Added JSON and button functionality

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const player = {};
 playArea.stats = document.querySelector('.stats');
 playArea.main = document.querySelector('.main');
 playArea.game = document.querySelector('.main');
-playArea.btns = document.querySelectorAll('.btn');
-Array.from(document.querySelectorAll('.btn'));
+playArea.btns = Array.from(document.querySelectorAll('.btn'));
 Array.from(document.querySelectorAll('.page'));
 console.log(playArea);
+
+player.score = 0;
+player.items = 3;
+
+playArea.btns.forEach(function (item) {
+  console.log(item);
+  item.addEventListener('click', handleBtn);
+});
+
+function handleBtn(event) {
+  event.preventDefault();
+  console.log(event.target.classList.contains('new-game'));
+  if (event.target.classList.contains('new-game')) {
+    console.log('yes');
+    startGame();
+  }
+}
+
+function startGame() {
+  console.log('Start');
+}
